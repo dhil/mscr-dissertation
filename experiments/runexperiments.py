d9e25be9500039d8a07efb9a74cb8e79798f74aa
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2016-07-10 00:18:42 +0100

    More fiddling with run experiments script

--- experiments/runexperiments.py
+++ experiments/runexperiments.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3.5
 
+import json
+import os
+
 # TODO:
+# opam switch
+# opam list
 # Read config
 # Setup environment
 # Dump printenv
@@ -14,12 +19,33 @@
 #   - Run X times; compute hash of each output; compare
 # Notify about results (e.g. e-mail)
 
-# Experiments context
-experiments_context = {'continue': True, 'name': "World"}
+settings = None
 
+# Load settings and experiments context
+with open("settings.json") as settings_file:
+    settings = json.load(settings_file)
+    print(settings)
+   
+# Experiments context
+#experiments_context = {'source': {'git': {'branch': 'effect-handlers-compilation', 'url': 'https://github.com/links-lang/links.git'} } }
+
+# Computes the hash of a given file
+def compute_hash(file):
+    BLOCKSIZE = 4096
+    hasher = hashlib.md5()
+    with open(file, 'rb') as afile:
+        buf = afile.read(BLOCKSIZE)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = afile.read(BLOCKSIZE)
+    return hasher.hexdigest()
+
+# Expands environment variables
+def expandvars(s):
+    return os.path.expandvars(s)
 
 def dump_command(cmd, ctxt):
-    print("+ " + str(cmd))
+    print("+ {:s}".format(cmd))
 
 # Pipeline pattern
 def run_pipeline(steps, seed):
@@ -28,6 +54,38 @@ def run_pipeline(steps, seed):
         value = step(value)
     return value
 
+def make_context(settings):
+    mySettings = settings.copy()
+    mySettings.update({'useCompilers': [0, 1, 2]})
+    return mySettings
+
+def make_experiment_config(settings):
+    settings.update({'useCompilers': [0, 1, 2]})
+    exps = []
+    for (compId, val) in enumerate(settings['useCompilers']):
+        compExps = []
+        for exp in settings['experiments']:
+            for prog in exp['programs']:
+                if (any(comp['id'] == compId for comp in prog['compilers'])):
+                    filename = expandvars(os.path.join(exp['directory'], prog['filename']))
+                    compExps.append({'filename': filename, 'checksum': prog['checksum']})
+        exps.append({'id': compId, 'programs': compExps})
+    print(exps)
+    exit(1)
+    settings.update({'experiments': exps})
+    return settings
+
+def setup_links_experiments(settings):
+    # git clone
+    # git log
+    # etc..
+    return ()
+
+def setup_environment():    
+    return ()
+
+def run_experiments(ctxt):
+    return ctxt
 
 def dump_date(ctxt):
     cmd = "date +'%Y-%m-%d %H:%M:%S %Z%z [%A, %B]'"
@@ -55,7 +113,10 @@ def dump_who(ctxt):
     return ctxt
 
 def git_clone_project(ctxt):
-    cmd = "git clone -b effect-handlers-compilation https://github.com/links-lang/links.git"
+    git = ctxt['settings']['source']['git']
+    git_url = git['url']
+    branch  = git['branch']
+    cmd = "git clone -b {:s} {:s}".format(branch, git_url)
     dump_command(cmd, ctxt)
     return ctxt
 
@@ -68,18 +129,9 @@ def build_project(ctxt):
     cmd = "make nc"
     dump_command(cmd, ctxt)
     return ctxt
-
-def run_experiments(ctxt):
-    return ctxt
-
-def print_hello(ctxt):
-    if ctxt['continue']:
-        print("Hello " + str(ctxt['name']))
-        ctxt.update({'name': "Foobar"})
-        ctxt.update({'continue': False})
-    return ctxt
-        
+       
 methodology = [
+    make_experiment_config,
     dump_date,
     dump_printenv,
     dump_hostname,
@@ -91,4 +143,4 @@ methodology = [
     run_experiments
  ]
 
-run_pipeline(methodology, experiments_context)
+run_pipeline(methodology, settings)
