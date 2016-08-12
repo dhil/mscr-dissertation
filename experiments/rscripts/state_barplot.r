19fdc19ac4076cc9ed71d656d7ea90a2e98f0585
Daniel Hillerström <dhildotnet@gmail.com>
2016-08-12 12:31:18 +0100

    State plot

--- experiments/rscripts/state_barplot.r
+++ experiments/rscripts/state_barplot.r
@@ -5,32 +5,37 @@ library(reshape2)
 library(plyr)
 
 # This a comment
-compilerfile = "3/compiler.transposed.csv"
-interpreterfile = "3/interpreter.transposed.csv"
+compilerfile = "12/compiler.transposed.csv"
+interpreterfile = "12/interpreter.transposed.csv"
+ocamloptfile = "12/ocamlopt.transposed.csv"
 # Read data
 compilerDataset = read.csv(paste("/home/dhil/projects/mscr-dissertation/experiments/results", compilerfile, sep="/"), header = TRUE)
 interpreterDataset = read.csv(paste("/home/dhil/projects/mscr-dissertation/experiments/results", interpreterfile, sep="/"), header = TRUE)
+ocamloptDataset = read.csv(paste("/home/dhil/projects/mscr-dissertation/experiments/results", ocamloptfile, sep="/"), header = TRUE)
 
 # Interpreter data
 myIData <- list(c(interpreterDataset$state.links), c(interpreterDataset$monadicstate.links), c(interpreterDataset$purestate.links))
 myIData <- sapply(myIData, median)
 
-baseline = myIData[3]
-myIData.normalised <- sapply(myIData, {function(x) baseline / x})
-
-print(myIData.normalised)
-
 # Compiler data
 myCData <- list(c(compilerDataset$state.links), c(compilerDataset$monadicstate.links), c(compilerDataset$purestate.links))
 myCData <- sapply(myCData, median)
 
-#baseline = myIData[3]
+# OCamlopt data
+myOData <- list(c(ocamloptDataset$state.ml), c(ocamloptDataset$monadicstate.ml), c(ocamloptDataset$purestate.ml))
+myOData <- sapply(myOData, median)
+
+# Data normalisation
+baseline = myOData[3]
+
+myIData.normalised <- sapply(myIData, {function(x) baseline / x})
+print(myIData.normalised)
+
 myCData.normalised <- sapply(myCData, {function(x) baseline / x})
 print(myCData.normalised)
 
-#myCStateData <- as.data.frame(list(variable=stateLabels, executiontime=myCData.normalised))
-#myCStateData = melt(myCStateData, id.var="variable", variable.name="status")
-#print(comp.data.normalised)
+myOData.normalised <- sapply(myOData, {function(x) baseline / x})
+
 
 #employee <- c('John Doe','Peter Gynn','Jolie Hope')
 #salary <- c(21000, 23400, 26800)
@@ -41,10 +46,10 @@ print(myCData.normalised)
 
 # Reshape data
 benchmarks = c("Handler", "Monadic", "Pure")
-handler = c(myCData.normalised[1], myIData.normalised[1], 0.0)
-monadic = c(myCData.normalised[2], myIData.normalised[2], 0.0)
-pure = c(myCData.normalised[3], myIData.normalised[3], 0.0)
-names = c("compiler", "interpreter", "ocamlopt")
+handler = c(myCData.normalised[1], myIData.normalised[1], myOData.normalised[1])
+monadic = c(myCData.normalised[2], myIData.normalised[2], myOData.normalised[2])
+pure = c(myCData.normalised[3], myIData.normalised[3], myOData.normalised[3])
+names = c("Links compiler", "Links interpreter", "ocamlopt")
 data <- data.frame(names, handler, monadic, pure)
 #data = data[,c(1,2,3,4)]
 print(data)
